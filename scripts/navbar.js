f49a0a7b2cf7f009a0a439f7daaa78041905f69a
dburnham1212 <dburnham1212@users.noreply.github.com>
2024-11-30 23:52:16 -0500

    Add files via upload

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,10 +1,10 @@
 document.getElementById("navbar").innerHTML = `
     <div id="left"> 
         <span>CVRATDB</span>
-        <a href="./pages/index.html">Home</a>
+        <a href="./index.html">Home</a>
     </div>
     <div id="right">
-        <a href="./pages/ontario.html">Ontario</a>
-        <a href="./pages/manitoba.html">Manitoba</a>
+        <a href="./ontario.html">Ontario</a>
+        <a href="./manitoba.html">Manitoba</a>
     </div>
 `;
