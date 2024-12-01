5774087daa7570ce9279e42cbcd1cae14e2103f1
dburnham1212 <dburnham1212@users.noreply.github.com>
2024-11-30 23:45:29 -0500

    Delete DemoSite directory

--- DemoSite/scripts/loadXML.js
+++
@@ -1,9 +0,0 @@
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-if (this.readyState == 4 && this.status == 200) {
-myFunction(this);
-}
-};
-xhttp.open("GET", "example.xml", true);
-xhttp.send();
-
